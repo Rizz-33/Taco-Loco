a6f00ff753122db897940c9687c7eec80083944f
Rizz-33 <risiniamarathunga@gmail.com>
2024-03-07 19:33:57 +0530

    fix: errors

--- lib/intro_page.dart
+++ lib/intro_page.dart
@@ -12,51 +12,49 @@ class IntroPage extends StatelessWidget {
       backgroundColor: Color.fromARGB(255, 255, 242, 242),
       body: Padding(
         padding: EdgeInsets.all(25.0),
-        child: Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center, // Adjusted mainAxisAlignment
-            children: [
-              SizedBox(height: 25),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 25),
 
-              // icon
-              Padding(
-                padding: EdgeInsets.all(50.0),
-                child: Image.asset('lib/images/taco-truck.png', width: 100),
+            // icon
+            Padding(
+              padding: EdgeInsets.all(50.0),
+              child: Image.asset('lib/images/taco-truck.png', width: 100),
+            ),
+
+            // shop name
+            Text(
+              'TacoLoco',
+              style: GoogleFonts.dmSerifDisplay(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                color: primaryColor,
               ),
+            ),
 
-              // shop name
-              Text(
-                'TacoLoco',
-                style: GoogleFonts.dmSerifDisplay(
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-                  color: primaryColor,
-                ),
+            // slogan
+            Text(
+              'Crave the Craze',
+              style: GoogleFonts.quicksand(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Color.fromRGBO(
+                    primaryColor.red, primaryColor.green, primaryColor.blue, 0.75),
               ),
+            ),
 
-              // slogan
-              Text(
-                'Crave the Craze',
-                style: GoogleFonts.quicksand(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Color.fromRGBO(
-                      primaryColor.red, primaryColor.green, primaryColor.blue, 0.75),
-                ),
-              ),
+            SizedBox(height: 65),
 
-              SizedBox(height: 65),
-
-              // button
-              Button(
-                text: 'Get Started',
-                ontap: () {
-                  Navigator.pushNamed(context, '/menu');
-                },
-              ),
-            ],
-          ),
+            // button
+            Button(
+              text: 'Get Started',
+              ontap: () {
+                Navigator.pushNamed(context, '/menu');
+              },
+            ),
+          ],
         ),
       ),
     );
