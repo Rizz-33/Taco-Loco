e1441d34480875e78e8b0e4ba48b7f0e50996090
Rizz-33 <risiniamarathunga@gmail.com>
2024-03-07 21:30:55 +0530

    modify: import food

--- lib/models/shop.dart
+++ lib/models/shop.dart
@@ -1,5 +1,18 @@
 
 
+import 'package:tacoloco/models/food.dart';
+
 class Shop {
+
+  List<Food> foodMenu = [
+    //item 1
+    Food(name: 'Bacon, Egg, and Cheese\nBreakfast Taco', price: '45', imagePath: 'lib/images/Bacon, Egg, and ChEEse BrEakfast Taco.png', rating: '4.7'),
+
+    //item 2
+    Food(name: 'Bacon, Egg, and Cheese\nBreakfast Burrito', price: '45', imagePath: 'lib/images/Bacon, Egg, and Cheese Breakfast Burrito.png', rating: '4.7'),
+
+    //item 3
+    Food(name: 'Bacon, Potato, Egg and\nCheese Breakfast Taco', price: '45', imagePath: 'lib/images/BACON, POTATO, EGG & CHEESE BREAKFAST TACO.png', rating: '4.7'),
+  ];
 
 }
