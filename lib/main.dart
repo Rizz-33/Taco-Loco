350306ed43d585cf75fb48ffbeff830b892a5887
Rizz-33 <risiniamarathunga@gmail.com>
2024-03-07 21:51:04 +0530

    modify: main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:tacoloco/intro_page.dart';
 import 'package:tacoloco/menu.dart';
+import 'package:tacoloco/models/shop.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => Shop(),
+      child: const MyApp(),
+      )
+  );
 }
 
 class MyApp extends StatelessWidget {
