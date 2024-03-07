c742f0035c2d297969060dbe92d0f752c3c99dae
Rizz-33 <risiniamarathunga@gmail.com>
2024-03-07 21:38:48 +0530

    feat: add to cart method

--- lib/models/shop.dart
+++ lib/models/shop.dart
@@ -4,7 +4,7 @@ import 'package:tacoloco/models/food.dart';
 
 class Shop {
 
-  List<Food> foodMenu = [
+  List<Food> _foodMenu = [
     //item 1
     Food(name: 'Bacon, Egg, and Cheese\nBreakfast Taco', price: '45', imagePath: 'lib/images/Bacon, Egg, and ChEEse BrEakfast Taco.png', rating: '4.7'),
 
@@ -14,5 +14,22 @@ class Shop {
     //item 3
     Food(name: 'Bacon, Potato, Egg and\nCheese Breakfast Taco', price: '45', imagePath: 'lib/images/BACON, POTATO, EGG & CHEESE BREAKFAST TACO.png', rating: '4.7'),
   ];
+
+  //customer cart
+  List<Food> _cart = [];
+
+  //getter method
+  List<Food> get foodMenu => _foodMenu;
+  List<Food> get cart => _cart;
+
+  //add to cart
+  void addtocart(Food foodItem, int quantity){
+    for (int i = 0; 1< quantity; i++) {
+      _cart.add(foodItem);
+    }
+  }
+
+
+  //remove from cart
 
 }
