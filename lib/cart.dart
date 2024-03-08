d97d1e6b864aafb400c0b059972582dad5cb6d73
Rizz-33 <risiniamarathunga@gmail.com>
2024-03-08 10:11:22 +0530

    modify: listview

--- lib/cart.dart
+++ lib/cart.dart
@@ -25,32 +25,34 @@ class Cart extends StatelessWidget {
       body: Column(
         children: [
           //cart with items
-          ListView.builder(
-            itemCount: value.cart.length,
-            itemBuilder: (context, index){
-            //get food from cart
-            final Food food = value.cart[index];
-          
-            //get food name
-            final String foodName = food.name;
-          
-            //get food price
-            final String foodPrice = food.price;
-          
-            //retuen list tile
-            return Container(
-              decoration: BoxDecoration(color: primaryColor.withAlpha((primaryColor.alpha * 0.09).round()),
-              borderRadius: BorderRadius.circular(16)),
-              margin: EdgeInsets.only(left: 20, top: 20, right: 20),
-              child: ListTile(
-                title: Text(foodName,
-                style: TextStyle(fontWeight: FontWeight.bold),),
-                subtitle: Text(foodPrice,
-                style: TextStyle(color: Colors.grey[600]),),
-                trailing: IconButton(icon: Icon(Icons.delete, color: Colors.grey[500],), onPressed: removefromcart,),
-              ),
-            );
-          }),
+          Expanded(
+            child: ListView.builder(
+              itemCount: value.cart.length,
+              itemBuilder: (context, index){
+              //get food from cart
+              final Food food = value.cart[index];
+            
+              //get food name
+              final String foodName = food.name;
+            
+              //get food price
+              final String foodPrice = food.price;
+            
+              //retuen list tile
+              return Container(
+                decoration: BoxDecoration(color: primaryColor.withAlpha((primaryColor.alpha * 0.09).round()),
+                borderRadius: BorderRadius.circular(16)),
+                margin: EdgeInsets.only(left: 20, top: 20, right: 20),
+                child: ListTile(
+                  title: Text(foodName,
+                  style: TextStyle(fontWeight: FontWeight.bold),),
+                  subtitle: Text(foodPrice,
+                  style: TextStyle(color: Colors.grey[600]),),
+                  trailing: IconButton(icon: Icon(Icons.delete, color: Colors.grey[500],), onPressed: removefromcart,),
+                ),
+              );
+            }),
+          ),
 
           //pay button
           Button(text: 'Pay Now', ontap: (){})
