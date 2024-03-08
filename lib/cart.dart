425956bfef4bd80943e825e0f31c53803a5af8b0
Rizz-33 <risiniamarathunga@gmail.com>
2024-03-08 09:33:05 +0530

    feat: cart appbar title

--- lib/cart.dart
+++ lib/cart.dart
@@ -5,6 +5,10 @@ class Cart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Cart'),
+      ),
+    );
   }
 }
