19a3aa8bd4271a0a34cdd29d53cbcf1a5afa06f9
Rizz-33 <risiniamarathunga@gmail.com>
2024-03-08 10:10:25 +0530

    feat: pay now button

--- lib/cart.dart
+++ lib/cart.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:tacoloco/components/button.dart';
 import 'package:tacoloco/models/food.dart';
 import 'package:tacoloco/models/shop.dart';
 import 'package:tacoloco/themes/colors.dart';
@@ -21,32 +22,40 @@ class Cart extends StatelessWidget {
         backgroundColor: primaryColor,
         foregroundColor: Colors.white,
       ),
-      body: ListView.builder(
-        itemCount: value.cart.length,
-        itemBuilder: (context, index){
-        //get food from cart
-        final Food food = value.cart[index];
+      body: Column(
+        children: [
+          //cart with items
+          ListView.builder(
+            itemCount: value.cart.length,
+            itemBuilder: (context, index){
+            //get food from cart
+            final Food food = value.cart[index];
+          
+            //get food name
+            final String foodName = food.name;
+          
+            //get food price
+            final String foodPrice = food.price;
+          
+            //retuen list tile
+            return Container(
+              decoration: BoxDecoration(color: primaryColor.withAlpha((primaryColor.alpha * 0.09).round()),
+              borderRadius: BorderRadius.circular(16)),
+              margin: EdgeInsets.only(left: 20, top: 20, right: 20),
+              child: ListTile(
+                title: Text(foodName,
+                style: TextStyle(fontWeight: FontWeight.bold),),
+                subtitle: Text(foodPrice,
+                style: TextStyle(color: Colors.grey[600]),),
+                trailing: IconButton(icon: Icon(Icons.delete, color: Colors.grey[500],), onPressed: removefromcart,),
+              ),
+            );
+          }),
 
-        //get food name
-        final String foodName = food.name;
-
-        //get food price
-        final String foodPrice = food.price;
-
-        //retuen list tile
-        return Container(
-          decoration: BoxDecoration(color: primaryColor.withAlpha((primaryColor.alpha * 0.09).round()),
-          borderRadius: BorderRadius.circular(16)),
-          margin: EdgeInsets.only(left: 20, top: 20, right: 20),
-          child: ListTile(
-            title: Text(foodName,
-            style: TextStyle(fontWeight: FontWeight.bold),),
-            subtitle: Text(foodPrice,
-            style: TextStyle(color: Colors.grey[600]),),
-            trailing: IconButton(icon: Icon(Icons.delete, color: Colors.grey[500],), onPressed: removefromcart,),
-          ),
-        );
-      }),
+          //pay button
+          Button(text: 'Pay Now', ontap: (){})
+        ],
+      ),
     ),);
   }
 }
